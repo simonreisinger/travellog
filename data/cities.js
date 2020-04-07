42fa4290e51e9ab88dae1f508696fbe15cca359c
iPPON <15130192+ippon1@users.noreply.github.com>
2020-04-07 18:18:14 +0200

    new map added, cities added where I was

---
+++ data/cities.js
@@ -0,0 +1,69 @@
+var cities_visited = {
+    // Caribbean
+    Oranjestad: {Lat: 12.5268736, Lon: -70.0356845},
+    Willemstad: {Lat: 12.1091242, Lon: -68.9316546},
+    // North America
+    NYC: {Lat: 40.7130126953125, Lon: -74.00712585449219},
+    Chicago: {Lat: 41.878114, Lon: -87.629798},
+    Miami: {Lat: 25.761680, Lon: -80.191790},
+    KeyWest: {Lat: 24.555059, Lon: -81.779987},
+    LasVegas: {Lat: 36.169941, Lon: -115.139830},
+    LosAngeles: {Lat: 34.052234, Lon: -118.243685},
+    SanFrancisco: {Lat: 37.774929, Lon: -122.419416},
+    Toronto: {Lat: 43.653226, Lon: -79.383184},
+    Detroit: {Lat: 42.331427, Lon: -83.045754},
+
+    // Asia, Israel
+    Jerusalem: {Lat: 31.768319, Lon: 35.213710},
+    TelAviv: {Lat: 34.781768, Lon: 32.085300},
+
+    // Europe
+    Lissabon: {Lat: -9.139337, Lon: 38.722252},
+
+    Barcelona: {Lat: 41.385064, Lon: 2.173404},
+    SantiagodeCompostela: {Lat: 42.878213, Lon: -8.544844},
+    Palma: {Lat: 39.606111, Lon: 2.791184},
+    Avila: {Lat: 40.658836, Lon: -4.704552},
+    Segovia: {Lat: 40.942903, Lon: -4.108807},
+    Sevilla: {Lat: 37.3890926, Lon: -5.984459},
+    Fuerteventura: {Lat: 28.358744, Lon: -14.053676},
+    Lanzarote: {Lat: 28.685769, Lon: -14.754639},
+
+    Paris: {Lat: 48.856614, Lon: 2.352222},
+
+    London: {Lat: 51.507351, Lon: -0.127758},
+    Worthing: {Lat: 50.817870, Lon: -0.372882},
+    //Brighton: {Lat: 50.822530, Lon: -0.137163},
+    Portsmouth: {Lat: 50.816667, Lon: -1.083333},
+    //Arundel: {Lat: 0, Lon: 0},
+    Brighton: {Lat: 50.822530, Lon: -0.137163},
+    Gibraltar: {Lat: 36.140751, Lon: -5.353585},
+    Guernsey: {Lat: 49.465691, Lon: -2.585278},
+
+    Zurich: {Lat: 47.368650, Lon: 8.539183},
+    Munich: {Lat: 48.135125, Lon: 11.581981},
+    Graz: {Lat: 47.070714, Lon: 15.439504},
+    Salzburg: {Lat: 47.809490, Lon: 13.055010},
+
+    Lignano: {Lat: 45.668555, Lon: 13.104086},
+    Trieste: {Lat: 45.649526, Lon: 13.776818},
+    Bozen: {Lat: 46.498295, Lon: 11.354758},
+    Verona: {Lat: 45.438384, Lon: 10.991622},
+    Venice: {Lat: 45.440847, Lon: 12.315515},
+    Rome: {Lat: 41.902783, Lon: 12.496365},
+    Napoli: {Lat: 40.851775, Lon: 14.268124},
+    Florence: {Lat: 43.769560, Lon: 11.255814},
+    Pisa: {Lat: 10.382981, Lon: 10.382981},
+    Vernazza: {Lat: 44.134921, Lon: 9.684994},
+    Manarola: {Lat: 44.111559, Lon: 9.733894},
+    Mailand: {Lat: 45.465422, Lon: 9.185924},
+
+    Budapest: {Lat: 47.497912, Lon: 19.040235},
+    Krakow: {Lat: 50.064650, Lon: 19.944980},
+    Lublin: {Lat: 51.246910, Lon: 22.573620},
+
+    Samos: {Lat: 37.754787, Lon: 26.977770},
+    Chios: {Lat: 38.368183, Lon: 26.131006},
+    Crete: {Lat: 35.240116, Lon: 24.809269}
+
+};
