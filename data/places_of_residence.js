479029225b627a07fc13a1ca6eb66bf93741bbdb
SImon Reisinger <ippon1@riseup.net>
2020-04-12 01:03:51 +0200

    data added and code reduced

--- data/places_of_residence.js
+++ data/places_of_residence.js
@@ -1,6 +1,6 @@
 var place_of_residence = {
-    Urbana: {Lat: 40.11357498168945, Lon: -88.2201156616211},
-    Madrid: {Lat: 40.40453636336607, Lon: -3.6984651859070006},
-    Vienna: {Lat: 48.21627836300875, Lon: 16.335216951898186},
-    Ybbsitz: {Lat: 47.94738721047836, Lon: 14.894918951823408},
+    Urbana: {Lat: 40.11357498168945, Lon: -88.2201156616211, link: "https://simonreisinger.com/usa/", name: "Urbana"},
+    Madrid: {Lat: 40.40453636336607, Lon: -3.6984651859070006, link: "https://simonreisinger.com/spain/", name: "Madrid"},
+    Vienna: {Lat: 48.21627836300875, Lon: 16.335216951898186, link: "", name: "Vienna"},
+    Ybbsitz: {Lat: 47.94738721047836, Lon: 14.894918951823408, link: "https://simonreisinger.com/the-beauty-of-austria/", name: "Ybbsitz"},
 };
